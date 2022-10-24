a1345b5bd20dc2f8087b45f03c98f0ddb3ffcc41
sominlee74 <kunoctus@gmail.com>
2022-10-24 19:16:46 +0900

    linting config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-const DEV_MODE = true
-const BACKEND_PORT = 8888
+const DEV_MODE = true;
+const BACKEND_PORT = 8888;
 const REDIS_SERVER = {
-  host: '127.0.0.1',
-  port: 6379
-}
+  host: "127.0.0.1",
+  port: 6379,
+};
 
 let devMode = false;
-if (typeof DEV_MODE !== 'undefined') {
-  devMode = DEV_MODE || false
+if (typeof DEV_MODE !== "undefined") {
+  devMode = DEV_MODE || false;
 }
 
 module.exports = {
   devMode: devMode,
   backendPort: BACKEND_PORT,
-  redisServer: REDIS_SERVER
-}
+  redisServer: REDIS_SERVER,
+};
