e398dde8001f1c6ff72cb971b0999cacf0a99e2f
sominlee74 <kunoctus@gmail.com>
2022-10-25 18:51:37 +0900

    1st implementation server list & status

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const fileUpload = require("express-fileupload");
-const querystring = require("node:querystring");
-const config = require("./config/config");
-const session = require("./session");
-const debugging = require("./debugging");
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const querystring = require('node:querystring')
+const config = require('./config/config')
+// const session = require("./session");
+const debugging = require('./debugging')
+const healthCheck = require('./healthCheck')
 
 // Read/Set Config
 // const devMode = config.hasOwnProperty("devMode") ? config.devMode : false;
-const backendPort = config.hasOwnProperty("backendPort") ? config.backendPort : 8888;
+const backendPort = config.hasOwnProperty('backendPort') ? config.backendPort : 8888
 
 // Set Exxpress
-const app = express();
-app.use(express.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
-app.use(express.json()); // parsing application/json
-app.use(cors()); // CROS
-app.use(cookieParser()); // cookie-parser
-app.use(fileUpload()); // Prepare File-Upload
+const app = express()
+app.use(express.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
+app.use(express.json()) // parsing application/json
+app.use(cors()) // CROS
+app.use(cookieParser()) // cookie-parser
+app.use(fileUpload()) // Prepare File-Upload
 
 // DevMode only
-app.post("/backapi", (req, res) => debugging.rootPath(req, res));
-app.post("/backapi/viewCookie", async (req, res) => debugging.viewCookie(req, res));
-app.post("/backapi/viewSession", async (req, res) => debugging.viewSession(req, res));
+app.post('/backapi', (req, res) => debugging.rootPath(req, res))
+app.post('/backapi/viewCookie', async (req, res) => debugging.viewCookie(req, res))
+app.post('/backapi/viewSession', async (req, res) => debugging.viewSession(req, res))
 
+// get Server list and Status (req and res have JSON data)
+app.post('/backapi/server', async (req, res) => healthCheck.server_status(req, res))
+
+// Listening on port
 app.listen(backendPort, () => {
-  console.log(`listening on port ${backendPort}`);
-});
+    console.log(`listening on port ${backendPort}`)
+})
--- healthCheck.js
+++ healthCheck.js
@@ -1,37 +1,53 @@
-const tcpp = require("tcp-ping");
-const config = require("./config/config");
-const watchList = require("./config/watchList");
+const tcpp = require('tcp-ping')
+const config = require('./config/config')
+const watchList = require('./config/watchList')
 
-const isDevMode = config.hasOwnProperty("devMode") ? config.devMode : false;
-
-const server_list = isDevMode ? watchList.dev_server_list : watchList.test_server_list;
-// console.log('server_list', server_list);
+const isDevMode = config.hasOwnProperty('devMode') ? config.devMode : false
+const server_list = isDevMode ? watchList.dev_server_list : watchList.test_server_list
 
 const promisedProbe = (server) => {
-  return new Promise((resolve, reject) => {
-    tcpp.probe(server.address, server.port, (err, isAlive) => {
-      if (!err) {
-        resolve(isAlive);
-      }
-      reject(false);
-    });
-  });
-};
-
-async function server_check(server_list) {
-  const checkedList = [];
-  for (const server of server_list) {
-    const isAlive = await promisedProbe(server);
-    checkedList.push({
-      ...server,
-      isAlive: isAlive,
-    });
-  }
-  return checkedList;
+    return new Promise((resolve, reject) => {
+        tcpp.probe(server.address, server.port, (err, isAlive) => {
+            if (!err) {
+                resolve(isAlive)
+            }
+            reject(false)
+        })
+    })
 }
 
-(async () => {
-  let result_list = [];
-  result_list = await server_check(server_list);
-  console.log("result_list:", result_list);
-})();
+async function server_check(server_list) {
+    const checkedList = []
+    for (const server of server_list) {
+        const isAlive = await promisedProbe(server)
+        checkedList.push({
+            ...server,
+            isAlive: isAlive,
+        })
+    }
+    return checkedList
+}
+
+// (async () => {
+//     let result_list = []
+//     result_list = await server_check(server_list)
+//     console.log('result_list:', result_list)
+// })()
+
+async function server_status(req, res) {
+    if (!req.body.hasOwnProperty('address') || !req.body.hasOwnProperty('port')) {
+        res.send(await server_check(server_list))
+    } else {
+        const server = [
+            {
+                address: req.body.address,
+                port: req.body.port,
+            },
+        ]
+        res.send(await server_check(server))
+    }
+}
+
+module.exports = {
+    server_status,
+}
