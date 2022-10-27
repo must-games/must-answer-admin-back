3d29f2d8b01633006f65545d405d2cf45a90b6d5
sominlee74 <kunoctus@gmail.com>
2022-10-27 17:28:33 +0900

    Linting errorMessage.js

--- errorMessage.js
+++ errorMessage.js
@@ -1,25 +1,23 @@
-/** @format */
-
-const HTTP_STATUS_UNAUTHORIZED = 401;
-const HTTP_STATUS_NOT_DEV_MODE = 406;
+const HTTP_STATUS_UNAUTHORIZED = 401
+const HTTP_STATUS_NOT_DEV_MODE = 406
 
 class DevModeException extends Error {
-  constructor(message, apiPath, httpCode = HTTP_STATUS_NOT_DEV_MODE) {
-    super(message);
-    this.status = httpCode;
-    this.apiPath = apiPath;
-  }
+    constructor(message, apiPath, httpCode = HTTP_STATUS_NOT_DEV_MODE) {
+        super(message)
+        this.status = httpCode
+        this.apiPath = apiPath
+    }
 }
 
 class UnAuthorized extends Error {
-  constructor(message, apiPath = "viewSession", httpCode = HTTP_STATUS_UNAUTHORIZED) {
-    super(message);
-    this.status = httpCode;
-    this.apiPath = apiPath;
-  }
+    constructor(message, apiPath = 'viewSession', httpCode = HTTP_STATUS_UNAUTHORIZED) {
+        super(message)
+        this.status = httpCode
+        this.apiPath = apiPath
+    }
 }
 
 module.exports = {
-  DevModeException,
-  UnAuthorized,
-};
+    DevModeException,
+    UnAuthorized,
+}
