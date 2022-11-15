ac6deb7c4957f66f1ce1a13a6c7dd960ab1c38f5
sominlee74 <kunoctus@gmail.com>
2022-11-15 20:32:05 +0900

    Add prd server list

--- config/watchList.js
+++ config/watchList.js
@@ -1,4 +1,59 @@
-const server_list = []
+const server_list = [
+    {
+        category: 'Redis',
+        displayName: 'e01-prd-Redis',
+        address: '10.220.204.189',
+        port: 6379,
+    },
+    {
+        category: 'Web-Proxy',
+        displayName: 'w01-prd-Nginx',
+        address: '211.252.122.122',
+        port: 443,
+    },
+    {
+        category: 'Web-Proxy',
+        displayName: 'w02-prd-Nginx',
+        address: '211.252.122.107',
+        port: 443,
+    },
+    {
+        category: 'Game',
+        displayName: 'e03-prd-Game',
+        address: '10.220.204.248',
+        port: 8080,
+    },
+    {
+        category: 'Game',
+        displayName: 'e04-prd-Game',
+        address: '10.220.204.231',
+        port: 8080,
+    },
+    {
+        category: 'DataBase',
+        displayName: 'd02-prd-GameDB',
+        address: '10.220.205.70',
+        port: 3306,
+    },
+    {
+        category: 'Auth',
+        displayName: 'a01-prd-Auth',
+        address: '10.220.205.28',
+        port: 8080,
+    },
+    {
+        category: 'Auth',
+        displayName: 'a02-prd-Auth',
+        address: '10.220.205.30',
+        port: 8080,
+    },
+    {
+        category: 'DataBase',
+        displayName: 'd03-prd-AuthDB',
+        address: '10.220.205.92',
+        port: 3306,
+    },
+]
 
 const test_server_list = [
     {
